27202a61b3627c8438fdfa67a0ba446a09e78852
pzyu <pangzhengyu@live.com>
2016-08-19 03:34:32 +0800

    asfasf

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var packageInfo = require('./package.json');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 0.0.0.0));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
