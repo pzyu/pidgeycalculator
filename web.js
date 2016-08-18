e0c5e3b03697848af53c76b22726c987e394e69b
pzyu <pangzhengyu@live.com>
2016-08-19 03:37:22 +0800

    asfas

--- web.js
+++ web.js
@@ -2,15 +2,16 @@ var express = require('express');
 var packageInfo = require('./package.json');
 
 var app = express();
+var port = process.env.PORT || 0.0.0.0;
 
-app.set('port', (process.env.PORT || 0.0.0.0));
-app.use(express.static(__dirname + '/public'));
+//app.set('port', (process.env.PORT || 0.0.0.0));
+//app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(app.get('port'), function () {
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
