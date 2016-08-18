371ca700a397d710c16c2e907b36cb04c6bfe5d5
pzyu <pangzhengyu@live.com>
2016-08-19 03:25:25 +0800

    Port

--- web.js
+++ web.js
@@ -3,11 +3,14 @@ var packageInfo = require('./package.json');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT || 5000 || app.get('port'), function () {
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
 
